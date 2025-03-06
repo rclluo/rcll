0c8463aea9684cdce13341d3a789f12c90a3b444
rclluo <127714676+rclluo@users.noreply.github.com>
2025-03-05 20:50:19 -0800

    Add files via upload

--- mathematics/ge.py
+++ mathematics/ge.py
@@ -40,3 +40,51 @@ class Line():
 
     def __str__(self):
         return str({"slope":self.a,"y-intercept":self.b})
+
+class Polygon():
+    """
+    A Polygon
+    """
+    def __init__(self, verticies):
+        """
+        Verticies must be given in order, or sidelength and area returns will be inaccurate
+        """
+        if hasattr(verticies,"__iter__"):
+            self.verticies=[Vector2D(v) for v in verticies]
+            self.vertex_count=len(self.verticies)
+            self.sidelength=[(self.verticies[i]-self.verticies[i-1]).magnitude() for i in range(self.vertex_count)]
+        else:
+            raise TypeError("verticies must be an iterable")
+    
+    def area(self):
+        """
+        Signed area by the shoelace formula
+        """
+        return sum([
+            (self.verticies[i+1][0]-
+             self.verticies[i][0])*
+            (self.verticies[i+1][1]+
+             self.verticies[i][1]) 
+             for i in range(self.vertex_count)])/2
+
+class Triangle(Polygon):
+    """
+    A triangle
+    """
+    def __init__(self, verticies):
+        if len(verticies)==3:
+            super().__init__(verticies)
+        else:
+            raise ValueError("Must have 3 verticies")
+    
+    def semiperimeter(self):
+        return sum(self.sidelength)/2
+
+    def area_heron(self):
+        s=self.semiperimeter()
+        return sqrt(
+            s*
+            (s-self.sidelength[0])*
+            (s-self.sidelength[1])*
+            (s-self.sidelength[2])
+        )
