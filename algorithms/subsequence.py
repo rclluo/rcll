731047caeaaa3d20ec1219b2f4883472baa63946
rclluo <127714676+rclluo@users.noreply.github.com>
2025-03-05 16:19:23 -0800

    Add files via upload

--- algorithms/subsequence.py
+++ algorithms/subsequence.py
@@ -42,33 +42,27 @@ def lcs(a: list, b: list):
     """
     m=len(a)
     n=len(b)
-    l = [[0 for x in range(n+1)] for x in range(m+1)]
+    l=[[0 for x in range(n+1)] for x in range(m+1)]
     for i in range(m+1):
         for j in range(n+1):
-            if i == 0 or j == 0:
-                l[i][j] = 0
-            elif a[i-1] == b[j-1]:
-                l[i][j] = l[i-1][j-1] + 1
+            if i==0 or j==0:
+                l[i][j]=0
+            elif a[i-1]==b[j-1]:
+                l[i][j]=l[i-1][j-1]+1
             else:
-                l[i][j] = max(l[i-1][j], l[i][j-1])
-
-    index = l[m][n]
-
-    dp = [""] * (index+1)
-    dp[index] = ""
-
-    i = m
-    j = n
-    while i > 0 and j > 0:
-
-        if a[i-1] == b[j-1]:
-            dp[index-1] = a[i-1]
-            i -= 1
-            j -= 1
-            index -= 1
-
-        elif l[i-1][j] > l[i][j-1]:
-            i -= 1
+                l[i][j]=max(l[i-1][j], l[i][j-1])
+    index=l[m][n]
+    dp=[None]*(index+1)
+    i=m
+    j=n
+    while i>0 and j>0:
+        if a[i-1]==b[j-1]:
+            dp[index-1]=a[i-1]
+            i-=1
+            j-=1
+            index-=1
+        elif l[i-1][j]>l[i][j-1]:
+            i-=1
         else:
-            j -= 1
-    return dp
+            j-=1
+    return dp[:-1]
