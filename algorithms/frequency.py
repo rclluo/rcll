1b6d3dbf1cefe6d95c8108bb062aa2c971287a63
rclluo <127714676+rclluo@users.noreply.github.com>
2025-03-08 08:35:29 -0800

    Add files via upload

---
+++ algorithms/frequency.py
@@ -0,0 +1,12 @@
+"""
+Frequency Array
+"""
+
+def freq(ls: list, key: lambda a:a):
+    ret={}
+    for i in ls:
+        k=key(i)
+        if k in ret:
+            ret[k]+=1
+        else:
+            ret[k]=1
